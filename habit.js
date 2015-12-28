2484aa24534a12d7f02898cc4682ecd2b53260a9
randy brown <randy@beingbrown.net>
2015-12-27 16:29:07 -0800

    single file for all source
    This needs refactoring, but it's the easiest way to start with the current base

--- habit.js
+++ habit.js
@@ -1,6 +1,61 @@
 window.onload = function() {
     document.querySelector('#nojs').remove();
-}
+    var storage = getStorage();
+    var appMain = document.querySelector('main');
+    var habits = getHabits(appMain, storage); //should just be habits and actions
+
+    storage.init("habit", function() {    
+        storage.createTable("habit", function() {
+            storage.createTable("action", function() {
+                //this can soon be replaced with habit.init(), which will call render
+                storage.load("habit", function(list) {
+                    storage.load("action", function(actions) {
+                        habits.init(list, actions);
+                    }, function() {
+                        console.log("could not retrive actions");
+                    });
+                }, function() {
+                    console.log("could not retreve habits");
+                });
+            }, function() {
+                console.log("creating action table failed");
+            });
+        }, function() {
+            console.log("creating habit table failed");
+        });
+    }, function() {
+        console.log("indexeddb could not be initialized");
+    });
+};
+
+/**
+ * habit api:
+ * label: the text for the habit
+ * interval: the time interval for the habit in milliseconds
+ * expiration: the time the streak will expire
+ *   starting to second guess on expiration being a part of the habit
+ * streak: some way to count streaks
+ * history: record of when the habit has been 'completed' probably an array
+ * active: true or false to designate if a habit is active
+ * there probably needs to be a way to delete a habit as well
+**/
+
+/**
+ * habit table:
+ * id: a unique id assigned by indexeddb
+ * label: the text description of the habit
+ * interval: the frequency of the habit's execution
+ * createtime: the time the habit was added to the table
+ * active: whether or not the user is still wanting alto track the habit
+**/
+
+/**
+ * action table:
+ * id: a unique id assigned by indexeddb
+ * habit: the unique id of the habit from the habit table
+ * timestamp: a timestamp of when the action happened
+ * interval: the difference between this timestamp and the last
+**/
 
 function getHabits(aview, amodel) {
     var view = aview;
@@ -206,6 +261,96 @@ function getHabits(aview, amodel) {
                 });
                 //action.interval can be calculated and backfilled later
             });
+        }
+    };
+}
+
+function getStorage() {
+    var namespace;
+    var database;
+    return {
+        init: function(aNamespace, success, failure) {
+            if(window.indexedDB) {
+                namespace = aNamespace;
+                var request = indexedDB.open(namespace);
+                request.onsuccess = function(event) {
+                    database = request.result;
+                    success(null);
+                };
+                request.onfailure = function(event) {
+                    failure(null);
+                };
+            } else {
+                failure(null);
+            }
+        },
+        createTable: function(table, success, failure) {
+            if(!database) {
+                failure(null);
+            }
+            var exists = false;
+            for(var i = 0; i < database.objectStoreNames.length && !exists; i++) {
+                exists = database.objectStoreNames[i] === table;
+            }
+            if(exists) {
+                success(null);
+            } else {
+                var version = database.version + 1;
+                database.close();
+                var request = indexedDB.open(namespace, version);
+                request.onsuccess = function(event) {
+                    success(null);
+                };
+                request.onerror = function(event) {
+                    failure(null);
+                };
+                request.onupgradeneeded = function(event) {
+                    database = event.target.result;
+                    var objectStore = database.createObjectStore(table, { keyPath: "id", autoIncrement: true });
+                };
+            }
+        },
+        save: function(table, obj, success, failure) {
+            if(!database) {
+                failure(null);
+            }
+            if(obj.id) {
+                delete obj.id;
+            } else {
+                obj.id = parseInt(obj.id);
+            }
+            var transaction = database.transaction([table], "readwrite");
+            transaction.oncomplete = function(event) {
+                success(obj);
+            };
+            transaction.onerror = function(event) {
+                failure(null);
+            };
+            var objectStore = transaction.objectStore(table);
+            var request = objectStore.put(obj);
+            request.onsuccess = function(event) {
+                obj.id = event.target.result;
+            };
+            request.onerror = function(event) {
+                failure(null);
+            };
+        },
+        load: function(table, success, failure) {
+            if(!database) {
+                failure(null);
+            }
+            var result = [];
+            var transaction = database.transaction(table);
+            var objectStore = transaction.objectStore(table);
+            objectStore.openCursor().onsuccess = function(event) {
+                var cursor = event.target.result;
+                if(cursor) {
+                    result.push(cursor.value);
+                    cursor.continue();
+                } else {
+                    success(result);
+                }
+            };
         }
     };
 }
